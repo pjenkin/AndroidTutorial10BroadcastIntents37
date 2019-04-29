2bbeed1c7784a37edc4289b72dc528c670d9c534
Peter N Jenkin <peternjenkin@gmail.com>
2019-04-29 15:22:56 +0100

    38 Manifest altered, and code added, to process received broadcast Intent (also launch config to no activity, for the moment)

--- receive38/src/main/java/com/example/receive38/ReceiveBroadcast.java
+++ receive38/src/main/java/com/example/receive38/ReceiveBroadcast.java
@@ -3,6 +3,7 @@ package com.example.receive38;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class ReceiveBroadcast extends BroadcastReceiver {
 
@@ -10,6 +11,7 @@ public class ReceiveBroadcast extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Toast.makeText(context, "A broadcast has been received by this app!", Toast.LENGTH_LONG).show();
+        // throw new UnsupportedOperationException("Not yet implemented");  // boilerplate
     }
 }
