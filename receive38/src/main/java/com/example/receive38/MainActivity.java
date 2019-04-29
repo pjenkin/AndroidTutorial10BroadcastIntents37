69ac9656ab7b455057fcb5ad3060469c235cc046
Peter N Jenkin <peternjenkin@gmail.com>
2019-04-29 17:40:56 +0100

    38 Receiving broadcast intents, by including Intent.setAction parameter in IntentFilter.addAction

--- receive38/src/main/java/com/example/receive38/MainActivity.java
+++ receive38/src/main/java/com/example/receive38/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.receive38;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,9 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    // declare new broadcast receiver (qv) in Activity scope
+    ReceiveBroadcast broadcastReceiver = new ReceiveBroadcast();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +20,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.sendbroadcast37");
+        // include Intent.setAction parameter in IntentFilter.addAction
+        this.registerReceiver(broadcastReceiver, intentFilter);
     }
 
 }
